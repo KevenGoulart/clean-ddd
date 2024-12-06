72762335ed1a6c98ea98de64c777156f7c509269
KevenGoulart <kevengoulartmm@gmail.com>
2024-12-06 10:09:26 -0300

    initial commit

---
+++ src/domain/forum/application/use-cases/choose-question-best-answer.ts
@@ -0,0 +1,56 @@
+import { AnswersRepository } from '../repositories/answers-repository'
+import { QuestionsRepository } from '../repositories/questions-repository'
+import { Question } from '../../enterprise/entities/question'
+import { Either, left, right } from '@/core/either'
+import { ResourceNotFoundError } from '../../../../core/errors/errors/resource-not-found-error'
+import { NotAllowedError } from '../../../../core/errors/errors/not-allowed-error'
+
+interface ChooseQuestionBestAnswerCaseRequest {
+  authorId: string
+  answerId: string
+}
+
+type ChooseQuestionBestAnswerCaseResponse = Either<
+  ResourceNotFoundError | NotAllowedError,
+  {
+    question: Question
+  }
+>
+
+export class ChooseQuestionBestAnswerCase {
+  constructor(
+    private questionsRepository: QuestionsRepository,
+    private answersRepository: AnswersRepository,
+  ) {}
+
+  async execute({
+    answerId,
+    authorId,
+  }: ChooseQuestionBestAnswerCaseRequest): Promise<ChooseQuestionBestAnswerCaseResponse> {
+    const answer = await this.answersRepository.findById(answerId)
+
+    if (!answer) {
+      return left(new ResourceNotFoundError())
+    }
+
+    const question = await this.questionsRepository.findById(
+      answer.questionId.toString(),
+    )
+
+    if (!question) {
+      return left(new ResourceNotFoundError())
+    }
+
+    if (authorId !== question.authorId.toString()) {
+      return left(new NotAllowedError())
+    }
+
+    question.bestAnswerId = answer.id
+
+    await this.questionsRepository.save(question)
+
+    return right({
+      question,
+    })
+  }
+}
---
+++ src/domain/forum/application/use-cases/fetch-question-answers.spec.ts
@@ -0,0 +1,62 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { makeAnswer } from '../../../../../test/factories/make-answer'
+import { InMemoryAnswersRepository } from '../../../../../test/repositories/in-memory-answers-repository'
+import { FetchQuestionAnswersUseCase } from './fetch-question-answers'
+import { InMemoryAnswerAttachmentsRepository } from '../../../../../test/repositories/in-memory-answer-attachments-repository'
+
+let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: FetchQuestionAnswersUseCase
+
+describe('Fetch question answers', () => {
+  beforeEach(() => {
+    inMemoryAnswerAttachmentsRepository =
+      new InMemoryAnswerAttachmentsRepository()
+    inMemoryAnswersRepository = new InMemoryAnswersRepository(
+      inMemoryAnswerAttachmentsRepository,
+    )
+    sut = new FetchQuestionAnswersUseCase(inMemoryAnswersRepository)
+  })
+
+  it('it should be able to fetch recent questions', async () => {
+    await inMemoryAnswersRepository.create(
+      makeAnswer({
+        questionId: new UniqueEntityID('question-1'),
+      }),
+    )
+    await inMemoryAnswersRepository.create(
+      makeAnswer({
+        questionId: new UniqueEntityID('question-1'),
+      }),
+    )
+    await inMemoryAnswersRepository.create(
+      makeAnswer({
+        questionId: new UniqueEntityID('question-1'),
+      }),
+    )
+
+    const result = await sut.execute({
+      questionId: 'question-1',
+      page: 1,
+    })
+
+    expect(result.value?.answers).toHaveLength(3)
+  })
+
+  it('it should be able to fetch paginated questions answers', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await inMemoryAnswersRepository.create(
+        makeAnswer({
+          questionId: new UniqueEntityID('question-1'),
+        }),
+      )
+    }
+
+    const result = await sut.execute({
+      questionId: 'question-1',
+      page: 2,
+    })
+
+    expect(result.value?.answers).toHaveLength(2)
+  })
+})
